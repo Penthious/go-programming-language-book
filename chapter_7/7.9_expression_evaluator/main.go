789ada22a5978bf35a269a37c1c7afc89c97b1d7
Penthious <tleffew1994@gmail.com>
2021-08-08 11:47:43 -0600

    finished up to chapter 12

---
+++ chapter_7/7.9_expression_evaluator/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+type Expr interface {
+	Eval(env Env) float64
+	Check(vars map[Var]bool) error
+}
+type Var string // idetifies a varible, eg x
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
+type literal float64 // numeric constant, EG 3.141
+func (l literal) Check(vars map[Var]bool) error {
+	return nil
+}
+
+type unary struct {
+	op rune // one of '+', '-'
+	x  Expr
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
+}
+
+type binary struct {
+	op   rune // one of `+`, `-`, `*`, `/`
+	x, y Expr
+}
+
+func (b binary) Check(vars map[Var]bool) error {
+	return nil
+}
+
+type call struct {
+	fn   string // one of `pow`, `sin`, `sqrt`
+	args []Expr
+}
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d", c.fn, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
+
+type Env map[Var]float64
+
+func (v Var) Eval(env Env) float64 {
+	return env[v]
+}
+func (l literal) Eval(_ Env) float64 {
+	return float64(l)
+}
+
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
+	case '+':
+		return +u.x.Eval(env)
+	case '-':
+		return -u.x.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (b binary) Eval(env Env) float64 {
+	switch b.op {
+	case '+':
+		return b.x.Eval(env) + b.y.Eval(env)
+	case '-':
+		return b.x.Eval(env) - b.y.Eval(env)
+	case '*':
+		return b.x.Eval(env) * b.y.Eval(env)
+	case '/':
+		return b.x.Eval(env) / b.y.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
+}
+func (c call) Eval(env Env) float64 {
+	switch c.fn {
+	case "pow":
+		return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
+	case "sin":
+		return math.Sin(c.args[0].Eval(env))
+	case "sqrt":
+		return math.Sqrt(c.args[0].Eval(env))
+	}
+	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
+}
+
+func main() {
+
+}
